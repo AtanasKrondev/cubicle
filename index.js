55e996a082507872e2dc37ddf57ae9906f104791
Atanas Krondev <krondev@gmail.com>
2019-09-28 12:41:21 +0300

    model cube created

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const env = process.env.NODE_ENV || 'development';
+global.__basedir = __dirname;
 
-const config = require('./config/config')[env];
-const app = require('express')();
+const cubeModel = require('./models/Cube');
 
-require('./config/express')(app);
-require('./config/routes')(app);
+cubeModel.insert({ name: 'Kubcho', difficulty: 'Lesen', price: 'Bezcenen' })
+    .then((inserted) => {
+        console.log(inserted);
+        return cubeModel.delete(inserted.id);
+    })
+    .then((deletedCube) => {
+        console.log('Bravo we');
+        console.log(deletedCube);
+    });
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+// const config = require('./config/config')[env];
+// const app = require('express')();
+
+// require('./config/express')(app);
+// require('./config/routes')(app);
+
+// app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
