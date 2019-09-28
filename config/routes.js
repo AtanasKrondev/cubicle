865d1473c9dff6a8c2a9cda6f76a78e33beb96f8
Atanas Krondev <krondev@gmail.com>
2019-09-28 15:34:12 +0300

    Create new entity

--- config/routes.js
+++ config/routes.js
@@ -2,5 +2,9 @@ const cubeController = require('../controllers/cube')
 
 module.exports = (app) => {
     app.get('/', cubeController.index);
-    app.get('/details/:id', cubeController.details)
+    app.get('/details/:id', cubeController.details);
+    app.get('/about', cubeController.about);
+    app.get('/create', cubeController.createGet);
+    app.post('/create', cubeController.createPost)
+    app.get('*', cubeController.notFound);
 };
--- controllers/cube.js
+++ controllers/cube.js
@@ -12,12 +12,41 @@ function details(req, res, next) {
     const id = +req.params.id;
     cubeModel.getOne(id)
         .then(cube => {
+            if (!cube) {
+                res.redirect('/not-found');
+                return;
+            }
             res.render('details.hbs', { cube })
         })
         .catch(next);
 }
 
+function notFound(req, res) {
+    res.render('404.hbs');
+}
+
+function about(req, res) {
+    res.render('about.hbs');
+}
+
+function createGet(req, res) {
+    res.render('create.hbs')
+}
+
+function createPost(req, res) {
+    const { name, description, imageUrl, difficultyLevel } = req.body;
+    const newCube = cubeModel.create(name, description, imageUrl, difficultyLevel);
+    cubeModel.insert(newCube)
+        .then(() => {
+            res.redirect('/');
+        });
+}
+
 module.exports = {
     index,
-    details
+    details,
+    notFound,
+    about,
+    createGet,
+    createPost
 }
